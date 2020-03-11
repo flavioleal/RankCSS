101264e4ec552a6c861405b7c445df38a8260ea0
JOAO VITOR <jvsistemas@gmail.com>
2020-03-11 13:35:52 -0300

    Ajustes pelo Flávio

--- app/src/app/rank-geral/rank-geral.component.ts
+++ app/src/app/rank-geral/rank-geral.component.ts
@@ -1,14 +1,6 @@
 import {Component, Injectable, OnInit} from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http'; 
 
-export interface Food {
-  calories: number;
-  carbs: number;
-  fat: number;
-  name: string;
-  protein: number;
-}
-
 @Component({
   selector: 'app-rank-geral',
   templateUrl: './rank-geral.component.html',
@@ -17,15 +9,8 @@ export interface Food {
 
 export class RankGeralComponent implements OnInit {
 
-  // dataSource: Food[] = [
-  //   {name: 'Yogurt', calories: 159, fat: 6, carbs: 24, protein: 4},
-  //   {name: 'Sandwich', calories: 237, fat: 9, carbs: 37, protein: 4},
-  //   {name: 'Eclairs', calories: 262, fat: 16, carbs: 24, protein: 6},
-  //   {name: 'Cupcakes', calories: 305, fat: 4, carbs: 67, protein: 4},
-  //   {name: 'Gingerbreads', calories: 356, fat: 16, carbs: 49, protein: 4},
-  // ];
-  displayedColumns: string[] = ['nickname', 'kill', 'death', 'assistance'];
-  lstRankDiario: Food[];
+  displayedColumns: string[] = ['index', 'nickname', 'kill', 'death', 'assistance', 'pontuation'];
+  lstRankDiario: any[];
 
   constructor(private http: HttpClient) { }
 
@@ -36,7 +21,6 @@ export class RankGeralComponent implements OnInit {
   public carregarRankDiario() {
     this.http.post<any>('http://localhost:56559/RankDiario', { title: 'Retorna Rank Diário' }).subscribe(data => {
       this.lstRankDiario = data;       
-      console.log(this.lstRankDiario);
     })
   }
 
