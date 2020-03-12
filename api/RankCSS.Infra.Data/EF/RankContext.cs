ce936efe52e8cec4f8c6b41c2c391abf901aa33b
Flávio Leal <flasile@gmail.com>
2020-03-12 13:52:45 -0300

    adicionando o context

---
+++ api/RankCSS.Infra.Data/EF/RankContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RankCSS.Infra.Data.EF
+{
+    public class RankContext : DbContext
+    {
+        public RankContext(DbContextOptions<RankContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
